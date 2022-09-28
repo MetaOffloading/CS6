778c886b476553f38da2bdd18ea3d75d383472fd
Sam Gilbert <sam.gilbert@ucl.ac.uk>
2022-09-28 12:19:07 +0100

    fixed issue with prolific ID in qualtrics link at end

--- WebTasks1/src/com/sam/webtasks/client/Instructions.java
+++ WebTasks1/src/com/sam/webtasks/client/Instructions.java
@@ -138,8 +138,9 @@ public class Instructions {
         case 10:
 			i = "You have now completed the experiment."
 					+ "Please click on the link below to return to the survey and complete the first session:<br><br>"
-					+ "<b><a href=\"https://uclpsych.eu.qualtrics.com/jfe/form/SV_dojGwYhhWyflvrE?PROLIFIC_PID={{%PROLIFIC_PID%}}\">"
-					+ "CLICK HERE</a></b>";
+					+ "<b><a href=\"https://uclpsych.eu.qualtrics.com/jfe/form/SV_dojGwYhhWyflvrE?PROLIFIC_PID=" 
+ 					+ SessionInfo.participantID + "\">"
+ 					+ "CLICK HERE</a></b>";
 			break;
 		}
 
